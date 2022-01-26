2be106d32fe94081d81b194d9bba2914f9b4b509
Tanmay Awasthi <tawasthi@groupon.com>
2022-01-26 21:44:48 +0530

    feat: more on routing

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,17 @@
 // route to our-domain/news
+import Link from 'next/link' // use this instead of anchor tag to support single page application
+import {Fragment} from "react";
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li><Link href='/news/nextjs-is-great'>NextJs is great</Link></li>
+        <li><Link href='/news/something-else'>Something Else</Link></li>
+      </ul>
+    </Fragment>
+  )
 }
 
 export default NewsPage;
